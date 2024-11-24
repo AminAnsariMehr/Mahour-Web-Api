ad7bd5a891dd4147934856b946cf1ce82e737ea7
Amin Ansari Mehr <aminansarimehrgeek23@gmail.com>
2024-11-24 22:38:09 +0330

    add uploadFileService to QuestionService

--- assets/js/Question.js
+++ assets/js/Question.js
@@ -9,204 +9,226 @@ const submitBtn = document.getElementById("submitBtn");
 let data, result, question;
 
 nextBtn.addEventListener("click", () => {
-    const targetQuestionID = getAnswers();
+	const targetQuestionID = getAnswers();
 
-    handleShowQuestion(targetQuestionID);
+	handleShowQuestion(targetQuestionID);
 });
 
 prevBtn.addEventListener("click", () => {
-    const [lastAnsweredQuestionID, lastAnsweredOptions] = Object.entries(
-        result.answers
-    ).at(-1);
+	const [lastAnsweredQuestionID, lastAnsweredOptions] = Object.entries(
+		result.answers
+	).at(-1);
 
-    if (Array.isArray(lastAnsweredOptions)) {
-        lastAnsweredOptions.forEach(option =>
-            updateServiceStatus(+lastAnsweredQuestionID, option, "prev")
-        );
-    } else if (typeof lastAnsweredOptions !== "string") {
-        updateServiceStatus(
-            +lastAnsweredQuestionID,
-            lastAnsweredOptions,
-            "prev"
-        );
-    }
+	if (Array.isArray(lastAnsweredOptions)) {
+		lastAnsweredOptions.forEach((option) =>
+			updateServiceStatus(+lastAnsweredQuestionID, option, "prev")
+		);
+	} else if (typeof lastAnsweredOptions !== "string") {
+		updateServiceStatus(
+			+lastAnsweredQuestionID,
+			lastAnsweredOptions,
+			"prev"
+		);
+	}
 
-    delete result.answers[lastAnsweredQuestionID];
-    handleShowQuestion(+lastAnsweredQuestionID);
+	delete result.answers[lastAnsweredQuestionID];
+	handleShowQuestion(+lastAnsweredQuestionID);
 });
 
 submitBtn.addEventListener("click", () => {
-    getAnswers();
-    console.log(result.answers);
+	getAnswers();
+	console.log(result.answers);
 });
 
 const getData = async () => {
-    try {
-        const formData = new FormData();
-        formData.append("service_id", "8");
-        formData.append("provider_id", "");
+	try {
+		const formData = new FormData();
+		formData.append("service_id", "58");
+		formData.append("provider_id", "");
 
-        const requestOptions = {
-            method: "POST",
-            body: formData,
-            redirect: "follow",
-        };
+		const requestOptions = {
+			method: "POST",
+			body: formData,
+			redirect: "follow",
+		};
 
-        const response = await fetch(
-            "https://maahoura.com/api/v1/question/getQuestions",
-            requestOptions
-        );
-        const json = await response.json();
+		const response = await fetch(
+			"https://maahoura.com/api/v1/question/getQuestions",
+			requestOptions
+		);
+		const json = await response.json();
 
-        data = json.data;
-        result = {
-            price: +data.price,
-            company: "some",
-            time: data.time,
-            answers: {},
-        };
+		data = json.data;
+		result = {
+			price: +data.price,
+			company: "some",
+			time: data.time,
+			answers: {},
+		};
 
-        handleShowQuestion(data.questions[0].id);
-        updateServiceStatus(data.questions[0].id);
-    } catch (error) {
-        console.error(error);
-    }
-
+		handleShowQuestion(data.questions[0].id);
+		updateServiceStatus(data.questions[0].id);
+	} catch (error) {
+		console.error(error);
+	}
 };
 
-const handleShowQuestion = questionID => {
-    let questionIndex;
+const handleShowQuestion = (questionID) => {
+	let questionIndex;
 
-    question = data.questions.find((question, index) => {
-        if (question.id === questionID) {
-            questionIndex = index;
-            return true;
-        }
-    });
-    questionTitle.innerText = question.title;
+	question = data.questions.find((question, index) => {
+		if (question.id === questionID) {
+			questionIndex = index;
+			return true;
+		}
+	});
+	questionTitle.innerText = question.title;
 
-    if (question.type === 0) {
-        questionOptions.innerHTML = questionOption(
-            question.id,
-            null,
-            null,
-            question.type
-        );
-    } else {
-        questionOptions.innerHTML = question.options
-            .map(option =>
-                questionOption(
-                    question.id,
-                    option.id,
-                    option.title,
-                    question.type
-                )
-            )
-            .join("");
-    }
+	if (question.type === 0 || question.type === 2) {
+		questionOptions.innerHTML = questionOption(
+			question.id,
+			null,
+			null,
+			question.type
+		);
+	} else {
+		questionOptions.innerHTML = question.options
+			.map((option) =>
+				questionOption(
+					question.id,
+					option.id,
+					option.title,
+					question.type
+				)
+			)
+			.join("");
+	}
 
-    progressBar.setAttribute(
-        "style",
-        `--p: ${(360 / data.questions.length) * (questionIndex + 1)}deg`
-    );
+	progressBar.setAttribute(
+		"style",
+		`--p: ${(360 / data.questions.length) * (questionIndex + 1)}deg`
+	);
 
-    btnDisableEnable();
+	btnDisableEnable();
 };
 
 const updateServiceStatus = (questionID, optionID, status = "next") => {
-    if (questionID && optionID) {
-        const { price, time } = data.questions
-            .find(item => item.id === questionID)
-            .options.find(option => option.id === optionID);
+	if (questionID && optionID) {
+		const { price, time } = data.questions
+			.find((item) => item.id === questionID)
+			.options.find((option) => option.id === optionID);
 
-        if (status === "next") {
-            result.price += price;
-            result.time += time;
-        } else {
-            result.price -= price;
-            result.time -= time;
-        }
-    }
+		if (status === "next") {
+			result.price += price;
+			result.time += time;
+		} else {
+			result.price -= price;
+			result.time -= time;
+		}
+	}
 
-    servicePrice.innerText = result.price;
-    serviceTime.innerText = result.time;
+	servicePrice.innerText = result.price;
+	serviceTime.innerText = result.time;
 };
 
 const questionOption = (questionID, optionID, optionTitle, optionType) => {
-    if (optionType === 0) {
-        return `
+	if (optionType === 0) {
+		return `
     <img class="textArea-svg" src="./assets/svg/textArea.svg" alt="">
     <textarea name="${questionID}" id="${questionID}" cols="30" rows="10" placeholder="توضیحات: "></textarea>`;
-    } else if (optionType === 1) {
-        return `<div>
+	} else if (optionType === 1) {
+		return `<div>
           <input type="radio" name="${questionID}" id="${optionID}" value="${optionID}" />
           <label for="${optionID}" class="Header7">${optionTitle}</label>
         </div>`;
-    } else if (optionType === 2) {
-        return `<div>
-          <input type="file" name="${questionID}" id="${optionID}" value="${optionID}" multiple />
-          <label for="${optionID}" class="Header7">${optionTitle}</label>
-        </div>`;
-    } else if (optionType === 3) {
-        return `<div>
+	} else if (optionType === 2) {
+		return `<section class="productOrderData">
+				<section class="uploadDataSection">
+					<div class="uploadImageSection">
+						<img class="uploadImageSection__image" src="./assets/images/dynamicLogic/uploadIcon.png"
+							alt="bgUploadIcon">
+						<div class="uploadImageSection__textWrapper">
+							<p class="uploadImageSection__title">انتخاب تصویر</p>
+							<p class="uploadImageSection__description">
+								تصاویر مربوطه را از گالری یا دوربین انتخاب کنید
+							</p>
+						</div>
+					</div>
+					<section class="uploadInputsWrapper">
+						<label for="uploadFileBtn" class="uploadInputsWrapper__uploadBtn">
+							<img class="uploadInputsWrapper__uploadIcon"
+								src="./assets/images/dynamicLogic/folder-add.png">
+							<span class="uploadInputsWrapper__text">انتخاب از گالری</span>
+						</label>
+						<input id="uploadFileBtn" class="uploadInputsWrapper__uploadInvisibleInput" type="file" multiple>
+
+						<button class="uploadInputsWrapper__uploadBtn">
+							<img class="uploadInputsWrapper__uploadIcon" src="./assets/images/dynamicLogic/camera.png">
+							<span class="uploadInputsWrapper__text">باز کردن دوربین</span>
+						</button>
+					</section>
+				</section>
+			</section>`;
+	} else if (optionType === 3) {
+		return `<div>
     <input type="checkbox" name="${questionID}" id="${optionID}" value="${optionID}" />
     <label for="${optionID}" class="Header7">${optionTitle}</label>
   </div>`;
-    }
+	}
 };
 
 const btnDisableEnable = () => {
-    const indexOfQuestion = data.questions.findIndex(
-        ({ id }) => id === question.id
-    );
+	const indexOfQuestion = data.questions.findIndex(
+		({ id }) => id === question.id
+	);
 
-    prevBtn.removeAttribute("disabled");
-    nextBtn.removeAttribute("disabled");
-    submitBtn.setAttribute("disabled", true);
+	prevBtn.removeAttribute("disabled");
+	nextBtn.removeAttribute("disabled");
+	submitBtn.setAttribute("disabled", true);
 
-    if (indexOfQuestion === 0) {
-        prevBtn.setAttribute("disabled", true);
-    } else if (indexOfQuestion === data.questions.length - 1) {
-        nextBtn.setAttribute("disabled", true);
-        submitBtn.removeAttribute("disabled");
-    }
+	if (indexOfQuestion === 0) {
+		prevBtn.setAttribute("disabled", true);
+	} else if (indexOfQuestion === data.questions.length - 1) {
+		nextBtn.setAttribute("disabled", true);
+		submitBtn.removeAttribute("disabled");
+	}
 };
 
 const getAnswers = () => {
-    if (question.type === 0) {
-        const { value } = document.getElementById(question.id);
+	if (question.type === 0) {
+		const { value } = document.getElementById(question.id);
 
-        result.answers[question.id] = value;
+		result.answers[question.id] = value;
 
-        return question.target_question_id;
-    } else if (question.type === 1) {
-        const { name, value } = document.querySelector(
-            `[name="${question.id}"]:checked`
-        );
+		return question.target_question_id;
+	} else if (question.type === 1) {
+		const { name, value } = document.querySelector(
+			`[name="${question.id}"]:checked`
+		);
 
-        result.answers[name] = +value;
+		result.answers[name] = +value;
 
-        updateServiceStatus(+name, +value);
+		updateServiceStatus(+name, +value);
 
-        return (
-            question.options.find(({ id }) => id === +value)
-                .target_question_id ?? question.target_question_id
-        );
-    } else if (question.type === 3) {
-        const selectedOptions = document.querySelectorAll(
-            `[name="${question.id}"]:checked`
-        );
+		return (
+			question.options.find(({ id }) => id === +value)
+				.target_question_id ?? question.target_question_id
+		);
+	} else if (question.type === 3) {
+		const selectedOptions = document.querySelectorAll(
+			`[name="${question.id}"]:checked`
+		);
 
-        selectedOptions.forEach(({ name, value }) => {
-            result.answers[name] = result.answers[name]
-                ? [...result.answers[name], +value]
-                : [+value];
+		selectedOptions.forEach(({ name, value }) => {
+			result.answers[name] = result.answers[name]
+				? [...result.answers[name], +value]
+				: [+value];
 
-            updateServiceStatus(+name, +value);
-        });
+			updateServiceStatus(+name, +value);
+		});
 
-        return question.target_question_id;
-    }
+		return question.target_question_id;
+	}
 };
 
 getData();
